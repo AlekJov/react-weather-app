1b879d06eb845cf887d0ec5583a4dc0af7bd124e
AlekJov <50363977+AlekJov@users.noreply.github.com>
2022-08-10 16:20:56 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '166c83a84bmsha4cb68955b6af77p140546jsn5928c91ccad7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9933699fffa8b8802384f11f1f7fed87";
